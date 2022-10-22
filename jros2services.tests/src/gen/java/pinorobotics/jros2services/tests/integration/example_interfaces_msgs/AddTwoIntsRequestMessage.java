8665998e1cc08840d7f37a77aa36cbebc8d0f6ec
lambdaprime <intid@protonmail.com>
2022-10-22 20:03:55 +0000

    Adding missing action definitions

---
+++ jros2services.tests/src/gen/java/pinorobotics/jros2services/tests/integration/example_interfaces_msgs/AddTwoIntsRequestMessage.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2021 jros2services project
+ * 
+ * Website: https://github.com/pinorobotics/jros2services
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package pinorobotics.jros2services.tests.integration.example_interfaces_msgs;
+
+import id.jrosmessages.Message;
+import id.jrosmessages.MessageMetadata;
+import id.jrosmessages.RosInterfaceType;
+import id.xfunction.XJson;
+import java.util.Objects;
+
+/**
+ * Definition for example_interfaces/AddTwoInts_Request
+ *
+ * @author lambdaprime intid@protonmail.com
+ */
+@MessageMetadata(name = AddTwoIntsRequestMessage.NAME, interfaceType = RosInterfaceType.SERVICE)
+public class AddTwoIntsRequestMessage implements Message {
+
+    static final String NAME = "example_interfaces/AddTwoIntsServiceRequest";
+
+    public long a;
+
+    public long b;
+
+    public AddTwoIntsRequestMessage() {}
+
+    public AddTwoIntsRequestMessage(long a, long b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        var other = (AddTwoIntsRequestMessage) obj;
+        return Objects.equals(a, other.b) && Objects.equals(a, other.b);
+    }
+
+    @Override
+    public String toString() {
+        return XJson.asString(
+                "a", a,
+                "b", b);
+    }
+}
